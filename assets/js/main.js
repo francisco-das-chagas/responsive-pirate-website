3f3cad089955b098b2a5ac582e0db8d9f7253174
Francisco das Chagas <f.chagasdutra18@gmail.com>
2025-03-18 11:45:52 -0300

    adicionando arquivos e criação de header e menu

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+    
+// Menu show
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Menu show
+if(navClose) {
+  navClose.addEventListener('click', () => {
+      navMenu.classList.remove('show-menu')
+  })
+}
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+
+/*=============== ADD SHADOW HEADER ===============*/
+
